17a76058e69e6621c1f5272563382612900f93ba
Glitch (wit-hdip-comp-sci-2020-playlist-1-3) <none>
2020-07-21 22:23:02 +0000

    🗃🏵 Checkpoint ./controllers/playlist.js:17960072/659 ./views/playlist.hbs:17960072/138 ./routes.js:17960072/100 ./models/playlist-store.js:17960072/476 ./controllers/dashboard.js:17960072/748

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const router = express.Router();
+const playlist = require('./controllers/playlist.js');
 
 const dashboard = require("./controllers/dashboard.js");
 const about = require("./controllers/about.js");
@@ -9,5 +10,6 @@ const about = require("./controllers/about.js");
 router.get("/", dashboard.index);
 router.get("/dashboard", dashboard.index);
 router.get("/about", about.index);
+router.get('/playlist/:id', playlist.index);
 
 module.exports = router;
